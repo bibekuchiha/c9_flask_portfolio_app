f27c01839ea03356e84675d78cd4c3b05377e84c
Bibek Shah Shankhar <shahshankharbibek@gmail.com>
2021-05-19 11:44:11 +0545

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	return render_template('index.html', name=name)
+	new_name = str(name) + 'You are doing Great :)'
+	return render_template('index.html', name=new_name)
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
@@ -31,6 +32,20 @@ def add_numbers_post():
   	      	for str_num in request.form['text'].split():
   	      		total += int(str_num)
   	      	return render_template('add_numbers.html', result=str(total))
+  	      except ValueError:
+  	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
+	
+@app.route('/multiply_numbers',method=['GET','POST'])
+def multiply_numbers_post():
+	if request.method='GET':
+		return render_template('multiply_numbers.html')
+	elif request.method == 'POST':
+	      print(request.form['text'].split())
+  	      total = 1
+  	      try:
+  	      	for str_num in request.form['text'].split():
+  	      		total *= int(str_num)
+  	      	return render_template('multiply_numbers.html', result=str(total))
   	      except ValueError:
   	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
 
